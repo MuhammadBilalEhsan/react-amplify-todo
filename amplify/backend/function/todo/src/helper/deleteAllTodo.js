c29c54cfcd79b5008c8387ed887c1a78feb3949d
MuhammadBilalEhsan <bilalprogrammer09@gmail.com>
2023-02-28 20:42:10 +0500

    backend code refactored.

--- amplify/backend/function/todo/src/helper/deleteAllTodo.js
+++ amplify/backend/function/todo/src/helper/deleteAllTodo.js
@@ -1,4 +1,5 @@
-const { deleteBatch } = require("../lib/dynamoDB");
+const { createQueryParams, batchDeleteParams } = require(".");
+const { batchWrite, query } = require("../lib/dynamoDB");
 
 const todoTable = process.env.API_REACTAMPLIFYTODO_TODOTABLE_NAME;
 
@@ -6,9 +7,20 @@ exports.deleteAllTodo = async (ctx) => {
   try {
     const { userId } = ctx;
     const date = new Date().toISOString();
-    const res = await deleteBatch(userId, todoTable);
-    console.log("res batch", res);
-    return { id: "_", name: "_", createdAt: date, updatedAt: date, ...ctx };
+
+    const todoRes = await query(createQueryParams(todoTable, userId));
+    console.log("todoRes res", todoRes);
+
+    const todoItems = todoRes?.Items || [];
+    if (todoItems?.length) {
+      const res = await batchWrite(batchDeleteParams(todoTable, todoItems));
+      console.log("batchWrite res", res);
+      return { id: "_", name: "_", createdAt: date, updatedAt: date, ...ctx };
+    } else {
+      const error = "The list is already empty.";
+      console.log("error", error);
+      throw new Error(error);
+    }
   } catch (error) {
     console.log("error", error);
     throw new Error(error);
